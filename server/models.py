a6dffcb8288051398e78e02ff2762ba91ad7a8a5
Kevin Choi <kevinnwjchoi@gmail.com>
2024-06-24 15:26:33 -0400

    creating User class. Added bcrypt

--- server/models.py
+++ server/models.py
@@ -1,6 +1,18 @@
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    #serialize_rules = ("",)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
