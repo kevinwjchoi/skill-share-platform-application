378ba3f2e9838d66dc3598fbed4e6af05f4762bc
Kevin Choi <kevinnwjchoi@gmail.com>
2024-07-15 15:14:49 -0400

    added .env and requirement

--- server/config.py
+++ server/config.py
@@ -9,13 +9,15 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_bcrypt import Bcrypt
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 # Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 app.config['SECRET_KEY'] = os.urandom(12).hex()
