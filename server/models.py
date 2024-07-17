e40acd4949025ffc8238dbac74387e47b03dad45
Kevin Choi <kevinnwjchoi@gmail.com>
2024-07-17 13:03:24 -0400

    removed unnecessary import

--- server/models.py
+++ server/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates, relationship
 
 from config import db, bcrypt
